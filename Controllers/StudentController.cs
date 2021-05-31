f48502be4ef523708bc113167a214b4bea4d6b9d
25779613 <duk.sal01@gmail.com>
2021-05-31 13:55:06 +0200

    fixed find single user

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -9,48 +12,124 @@ using StudentApi.Modals.Commands;
 [ApiController]
 [Route("student")]
 public class StudentController : ControllerBase
-{   
+{
     private StudentSQLCommands studentSQLCommands;
-    
+
+    public StudentController(IConfiguration config)
+    {
+        studentSQLCommands = new StudentSQLCommands(config);
+    }
+
     [HttpPost]
     [Route("/createStudent")]
-    public Student createStudent(Student student)
+    public async Task<HttpStatusCode> createStudent(Student student)
     {
         string studentName = student.studentName;
         string studentEmail = student.studentEmail;
         string studentNumber = student.studentNumber;
         string subject = student.subject;
         string studentDetails = student.studentDetails;
-        Console.WriteLine(studentName);
-        return null;
+        HttpStatusCode statusCode = HttpStatusCode.Created;
+        DbDataReader studentReader = null;
+        try
+        {
+            studentReader = await studentSQLCommands.createStudent(studentName, studentEmail, studentNumber, subject, studentDetails);
+
+        }
+        catch (Exception)
+        {
+            statusCode = HttpStatusCode.UnavailableForLegalReasons;
+        }
+        await studentSQLCommands.CloseConnection();
+        //Console.WriteLine(studentName+""+studentEmail+""+studentNumber+""+subject+""+studentDetails);]
+        return statusCode;
     }
 
     [HttpGet]
     [Route("/get/all/students")]
-    public Student getAllStudents(Student student)
-    {   
-        
-        return null;
+    public async Task<List<Student>> getAllStudents()
+    {
+        List<Student> students = new List<Student>();
+        DbDataReader studentReader = null;
+        studentReader = await studentSQLCommands.findAllStudents();
+        while (await studentReader.ReadAsync())
+        {
+            students.Add(
+                new Student()
+                {
+                    studentID = Convert.ToInt16(studentReader.GetValue("studentID")),
+                    studentName = studentReader.GetValue("studentName").ToString(),
+                    studentEmail = studentReader.GetValue("studentEmail").ToString(),
+                    studentNumber = studentReader.GetValue("studentNumber").ToString(),
+                    subject = studentReader.GetValue("subject").ToString(),
+                    studentDetails = studentReader.GetValue("studentDetails").ToString(),
+                }
+            );
+        }
+        await studentSQLCommands.CloseConnection();
+        return students;
     }
 
-    [HttpPost]
+    [HttpGet]
     [Route("/get/student/{studentID}")]
-    public Student getStudent(Student student)
+    public async Task<Student> getStudent(string studentID)
     {
-        return null;
+        DbDataReader studentReader = null;
+
+        studentReader = await studentSQLCommands.findStudent(studentID);
+        Student student =null;
+        while(await studentReader.ReadAsync())
+        {
+         student = (new Student()
+        {
+            studentID = Convert.ToInt16(studentReader.GetValue("studentID").ToString()),
+            studentName = studentReader.GetValue("studentName").ToString(),
+            studentEmail = studentReader.GetValue("studentEmail").ToString(),
+            studentNumber = studentReader.GetValue("studentNumber").ToString(),
+            subject = studentReader.GetValue("subject").ToString(),
+            studentDetails = studentReader.GetValue("studentDetails").ToString(),
+
+        });
+        }
+       
+        await studentSQLCommands.CloseConnection();
+        return student;
     }
     [HttpPost]
     [Route("/deleteStudent/{studentID}")]
-    public Student delete(Student student)
+    public async Task<HttpStatusCode> delete(string studentID)
     {
-        return null;
+        HttpStatusCode httpStatusCode = HttpStatusCode.Created;
+        DbDataReader studentReader = null;
+        try
+        {
+            studentReader = await studentSQLCommands.deleteStudent(studentID);
+        }
+        catch (Exception)
+        {
+            httpStatusCode = HttpStatusCode.Conflict;
+        }
+        await studentSQLCommands.CloseConnection();
+        return httpStatusCode;
     }
-    
+
     [HttpPost]
     [Route("/editStudent/{studentID}")]
-    public Student edit(Student student)
+    public async Task<HttpStatusCode> edit(string studentID, string studentEmail, string subject, string studentDetails)
     {
-        return null;
+        DbDataReader studentReader = null;
+        HttpStatusCode httpStatusCode = HttpStatusCode.Created;
+        try
+        {
+            studentReader = await studentSQLCommands.updateStudent(studentID, studentEmail, subject, studentDetails);
+        }
+        catch (Exception)
+        {
+            httpStatusCode = HttpStatusCode.ExpectationFailed;
+        }
+
+        await studentSQLCommands.CloseConnection();
+        return httpStatusCode;
     }
 
 
--- Modals/Commands/StudentSQLCommands.cs
+++ Modals/Commands/StudentSQLCommands.cs
@@ -9,61 +9,61 @@ namespace StudentApi.Modals.Commands
 {
     public class StudentSQLCommands
     {
-       private MySqlConnection connection =null;
-       private MySqlCommand mySqlCommand = null;
+        private MySqlConnection connection = null;
+        private MySqlCommand mySqlCommand = null;
 
-       public StudentSQLCommands(IConfiguration configuration)
-    {
-        connection = new MySqlConnection(configuration.GetSection("ConnectionStrings").GetSection("Default").Value);
-    }
 
-    public async Task<DbDataReader> createStudent(string studentName,string studentEmail,string studentNumber,string subject,string studentDetails)
-    {
-        await connection.OpenAsync();
-        mySqlCommand = new MySqlCommand("INSERT INTO students(studentName,studentEmail,studentNumber,subject,studentDetails)VALUES('"+studentName+"','"+studentEmail+"','"+studentNumber+"','"+subject+"','"+studentDetails+"')",connection);
-        var result = await mySqlCommand.ExecuteReaderAsync();
-        await connection.CloseAsync();
+        public StudentSQLCommands(IConfiguration configuration)
+        {
+            connection = new MySqlConnection(configuration.GetSection("ConnectionStrings").GetSection("Default").Value);
+        }
 
-        return result;
-    }
+        public async Task<DbDataReader> createStudent(string studentName, string studentEmail, string studentNumber, string subject, string studentDetails)
+        {
+            await connection.OpenAsync();
+            mySqlCommand = new MySqlCommand("INSERT INTO students(studentName,studentEmail,studentNumber,subject,studentDetails)VALUES('" + studentName + "','" + studentEmail + "','" + studentNumber + "','" + subject + "','" + studentDetails + "')", connection);
+            var result = await mySqlCommand.ExecuteReaderAsync();
+            
+            return result;
+        }
 
-    public async Task<DbDataReader> updateStudent(string studentID,string studentEmail,string subject,string studentDetails){
-        await connection.OpenAsync();
-        mySqlCommand = new MySqlCommand("UPDATE students SET studentEmail='"+studentEmail+"',subject='"+subject+"',studentDetails='"+studentDetails+"'WHERE studentID='"+Convert.ToInt16(studentID)+"'",connection);
-        var result = await mySqlCommand.ExecuteReaderAsync();
-        await connection.CloseAsync();
+        public async Task<DbDataReader> updateStudent(string studentID, string studentEmail, string subject, string studentDetails)
+        {
+            await connection.OpenAsync();
+            mySqlCommand = new MySqlCommand("UPDATE students SET studentEmail='" + studentEmail + "',subject='" + subject + "',studentDetails='" + studentDetails + "'WHERE studentID='" + Convert.ToInt16(studentID) + "'", connection);
+            var result = await mySqlCommand.ExecuteReaderAsync();
+            return result;
+        }
 
-        return result;  
-    }
+        public async Task<DbDataReader> deleteStudent(string studentID)
+        {
+            await connection.OpenAsync();
+            mySqlCommand = new MySqlCommand("DELETE FROM students WHERE studentID='" + Convert.ToInt16(studentID) + "'", connection);
+            var result = await mySqlCommand.ExecuteReaderAsync();
+            return result;
+        }
 
-    public async Task<DbDataReader> deleteStudent(string studentID)
-    {
-        await connection.OpenAsync();
-        mySqlCommand = new MySqlCommand("DELETE FROM students WHERE studentID='"+Convert.ToInt16(studentID)+"'",connection);
-        var result = await mySqlCommand.ExecuteReaderAsync();
-        await connection.CloseAsync();
+        public async Task<DbDataReader> findAllStudents()
+        {
+            await connection.OpenAsync();
+            List<Student> students = new List<Student>();
+            mySqlCommand = new MySqlCommand("SELECT * FROM students", connection);
+            var result = await mySqlCommand.ExecuteReaderAsync();
+            return result;
+        }
 
-        return result;  
-    }
+        public async Task<DbDataReader> findStudent(string studentID)
+        {
+            await connection.OpenAsync();
+            mySqlCommand = new MySqlCommand("SELECT * FROM students WHERE studentID='" + Convert.ToInt16(studentID) + "'", connection);
+            var result = await mySqlCommand.ExecuteReaderAsync();
+            return result;
+        }
 
-    public async Task<DbDataReader> findAllStudents(){
-        
-        List<Student> students = new List<Student>();
-        await connection.OpenAsync();
-        mySqlCommand = new MySqlCommand("SELECT * FROM students",connection);
-        var result = await mySqlCommand.ExecuteReaderAsync();
-        await connection.CloseAsync();
+        public async Task CloseConnection()
+        {
+            await connection.CloseAsync();
+        }
 
-        return result;  
-    }    
-
-     public async Task<DbDataReader> findStudent(string studentID){
-        await connection.OpenAsync();
-        mySqlCommand = new MySqlCommand("SELECT * FROM students WHERE studentID='"+Convert.ToInt16(studentID)+"'",connection);
-        var result = await mySqlCommand.ExecuteReaderAsync();
-        await connection.CloseAsync();
-
-        return result;  
-    }        
     }
 }
